0720ed4b06bff940c3a19523bd0e353165829328
User1987031987 <user19870319@gmail.com>
2018-07-12 21:50:44 +0530

    My First Commit

---
+++ src/app/material-module/material-module.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+MatToolbarModule
+@NgModule({
+  imports: [
+    CommonModule,
+    MatTool
+  ],
+  declarations: []
+})
+export class MaterialModuleModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCX4KtfPPn1PP8PJSsKDnwfQvZiv8LA_b0',
+    authDomain: 'ikamgmt2018.firebaseapp.com',
+    databaseURL: 'https://ikamgmt2018.firebaseio.com',
+    projectId: 'ikamgmt2018',
+    storageBucket: '',
+    messagingSenderId: '715213233955'
+  }
+};
